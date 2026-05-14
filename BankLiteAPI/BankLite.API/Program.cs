13fba8de59fd4ce7c31b16ccad291c1d337cf043
NicholasXydis <267418142+NicholasXydis@users.noreply.github.com>
2026-05-14 12:06:31 -0400

    feat: add security headers middleware

--- BankLiteAPI/BankLite.API/Program.cs
+++ BankLiteAPI/BankLite.API/Program.cs
@@ -149,6 +149,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseMiddleware<SecurityHeadersMiddleware>();
+
 app.UseHttpsRedirection();
 
 app.UseCors("AllowFrontend");
