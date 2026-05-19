619e111413371eeffa7d1442e04ecba79da13a04
NicholasXydis <267418142+NicholasXydis@users.noreply.github.com>
2026-05-19 14:43:48 -0400

    fix: add Amount upper bound to TransferValidator and add Token MaxLength to ResetPasswordValidator

--- BankLite.Application/Validators/ResetPasswordValidator.cs
+++ BankLite.Application/Validators/ResetPasswordValidator.cs
@@ -8,7 +8,8 @@ namespace BankLite.Application.Validators
         public ResetPasswordValidator()
         {
             RuleFor(x => x.Token)
-                .NotEmpty().WithMessage("Reset token is required.");
+                .NotEmpty().WithMessage("Reset token is required.")
+                .MaximumLength(256);
             RuleFor(x => x.NewPassword)
                 .NotEmpty().WithMessage("New password is required.")
                 .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
--- BankLite.Application/Validators/TransferValidator.cs
+++ BankLite.Application/Validators/TransferValidator.cs
@@ -7,7 +7,7 @@ namespace BankLite.Application.Validators
     {
         public TransferValidator()
         {
-            RuleFor(x => x.Amount).GreaterThan(0);
+            RuleFor(x => x.Amount).GreaterThan(0).LessThanOrEqualTo(1000000);
             RuleFor(x => x.FromAccountId).NotEmpty();
             RuleFor(x => x.ToAccountId).NotEmpty().NotEqual(x => x.FromAccountId).WithMessage("Cannot transfer to the same account.");
         }
