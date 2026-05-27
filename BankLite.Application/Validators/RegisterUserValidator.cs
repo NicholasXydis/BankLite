555a65d5387f75408ab6b932e1846a943d8ae6f9
NicholasXydis <267418142+NicholasXydis@users.noreply.github.com>
2026-05-27 10:07:01 -0400

    feat: add letters-only validation to full name

--- BankLite.Application/Validators/RegisterUserValidator.cs
+++ BankLite.Application/Validators/RegisterUserValidator.cs
@@ -7,8 +7,11 @@ namespace BankLite.Application.Validators
     {
         public RegisterUserValidator()
         {
-            RuleFor(x => x.FullName).NotEmpty().MaximumLength(50)
-            .Must(x => x == x.Trim()).WithMessage("Full name cannot have leading or trailing spaces");
+            RuleFor(x => x.FullName)
+                .NotEmpty()
+                .MaximumLength(50)
+                .Must(x => x == x.Trim()).WithMessage("Full name cannot have leading or trailing spaces")
+                .Must(x => x.All(c => char.IsLetter(c) || char.IsWhiteSpace(c))).WithMessage("Full name can only contain letters and spaces.");
             RuleFor(x => x.Email).NotEmpty().Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$").WithMessage("Please enter a valid email address").MaximumLength(256);
             RuleFor(x => x.Password).NotEmpty().MinimumLength(8).MaximumLength(100);
         }
